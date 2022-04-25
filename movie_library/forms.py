8a3e00d36cb57909cd5424fae23489059c18d4de
Marvin Thomas <mjerodd77@gmail.com>
2022-04-24 20:40:52 -0400

    Adding user login/logout functionality

--- movie_library/forms.py
+++ movie_library/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, SubmitField
-from wtforms.validators import InputRequired, NumberRange
+from wtforms import IntegerField, StringField, SubmitField, TextAreaField, URLField, PasswordField
+from wtforms.validators import InputRequired, NumberRange, Email, Length, EqualTo
 
 
 class MovieForm(FlaskForm):
@@ -9,3 +9,42 @@ class MovieForm(FlaskForm):
     year = IntegerField("Year", validators=[InputRequired(), NumberRange(
         min=1878, message="Please enter a year in the formaty YYYY")])
     submit = SubmitField("Add Movie")
+
+
+class StringListField(TextAreaField):
+    def _value(self):
+        if self.data:
+            return "\n".join(self.data)
+        else:
+            return ""
+
+    def process_formdata(self, valuelist):
+        if valuelist and valuelist[0]:
+            self.data = [line.strip() for line in valuelist[0].split("\n")]
+        else:
+            self.data = []
+
+
+class ExtendedMovieForm(MovieForm):
+    cast = StringListField("Cast")
+    series = StringListField("Series")
+    tags = StringListField("Tags")
+    description = StringListField("Description")
+    video_link = URLField("Video Link")
+    submit = SubmitField("Submit")
+
+
+class RegisterForm(FlaskForm):
+    email = StringListField("Email", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=6,
+                                        message="Your password must be at least 6 characters long.")])
+    confirm_password = PasswordField("Confirm Password", validators=[InputRequired(), EqualTo(
+        "password", message="This password did not match the one in the password field."
+    )])
+    submit = SubmitField("Register")
+
+
+class LoginForm(FlaskForm):
+    email = StringListField("Email", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    submit = SubmitField("Login")
--- movie_library/routes.py
+++ movie_library/routes.py
@@ -1,17 +1,128 @@
-from flask import Blueprint, render_template, request, redirect, session, current_app, url_for
-from movie_library.forms import MovieForm
-from movie_library.models import Movie
+from flask import Blueprint, render_template, request, redirect, session, current_app, url_for, abort, flash
+from movie_library.forms import MovieForm, ExtendedMovieForm, RegisterForm, LoginForm
+from movie_library.models import Movie, User
+import datetime
+import functools
 import uuid
 from dataclasses import asdict
+from passlib.hash import pbkdf2_sha256
 
 pages = Blueprint("pages", __name__, template_folder="templates", static_folder="static")
 
 
+def login_required(route):
+    @functools.wraps(route)
+    def route_wrapper(*args, **kwargs):
+        if session.get("email") is None:
+            return redirect(url_for(".login"))
+
+        return route(*args, **kwargs)
+    return route_wrapper
+
+
 @pages.route("/")
+@login_required
 def index():
-    movie_data = current_app.db.movie.find({})
+    user_data = current_app.db.user.find_one({"email"> session["email"]})
+    user = User(**user_data)
+
+    movie_data = current_app.db.movie.find({"_id": {"$in": user.movies}})
     movies = [Movie(**movie) for movie in movie_data]
-    return render_template("index.html", title="Movies Watchlist", movies_data=[])
+    return render_template("index.html", title="Movies Watchlist", movies_data=movies)
+
+
+@pages.route("/register", methods=["GET", "POST"])
+def register():
+    if session.get("email"):
+        return redirect(url_for(".index"))
+
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(
+            _id = uuid.uuid4().hex,
+            email = form.email.data,
+            password=pbkdf2_sha256.hash(form.password.data),
+        )
+        current_app.db.user.insert_one(asdict(user))
+
+        flash("User registered successfully", "success")
+
+        return redirect(url_for(".login"))
+
+    return render_template("register.html", title="Movies Watchlist - Register", form=form)
+
+
+@pages.route("/login", methods=["GET", "POST"])
+def login():
+    if session.get("email"):
+        return redirect(url_for(".index"))
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user_data = current_app.db.user.find_one({"email": form.email.data})
+        if not user_data:
+            flash("Login credentials not correct", category="danger")
+            return redirect(url_for(".login"))
+        user = User(**user_data)
+        if user and pbkdf2_sha256.verify(form.password.data, user.password):
+            session["user_id"] = user._id
+            session["email"] = user.email
+            return redirect(url_for(".index"))
+
+        flash("Login Failed...Please Try Again", category="danger")
+
+    return render_template("login.html", title="Movie Watchlist - Login", form=form)
+
+
+@pages.route("/logout")
+def logout():
+    session.clear()
+
+    return redirect(url_for(".login"))
+
+
+
+@pages.route("/edit/<string:_id>", methods=["GET", "POST"])
+@login_required
+def edit_movie(_id: str):
+    movie = Movie(**current_app.db.movie.find_one({"_id": _id}))
+    form = ExtendedMovieForm(obj=movie)
+    if form.validate_on_submit():
+        movie.cast = form.cast.data
+        movie.series = form.series.data
+        movie.tags = form.tags.data
+        movie.description = form.description.data
+        movie.video_link = form.video_link.data
+
+        current_app.db.movie.update_one({"_id": movie._id}, {"$set": asdict(movie)})
+        return redirect(url_for(".movie", _id=movie._id))
+    return render_template("movie_form.html", movie=movie, form=form)
+
+
+@pages.get("/movie/<string:_id>")
+def movie(_id: str):
+    movie_data = current_app.db.movie.find_one({"_id" : _id})
+    if not movie_data:
+        abort(404)
+    movie = Movie(**movie_data)
+    return render_template("movie_details.html", movie=movie)
+
+
+@pages.get("/movie/<string:_id>/rate")
+@login_required
+def rate_movie(_id):
+    rating = int(request.args.get("rating"))
+    current_app.db.movie.update_one({"_id": _id}, {"$set": {"rating": rating}})
+
+    return redirect(url_for(".movie", _id=_id))
+
+
+@pages.get("/movie/<string:_id>/watch")
+@login_required
+def watch_today(_id):
+    current_app.db.movie.update_one({"_id": _id}, {"$set": {"last_watched": datetime.datetime.today()}})
+    return redirect(url_for(".movie",_id=_id))
 
 
 @pages.route("/add", methods=["GET", "POST"])
@@ -27,9 +138,11 @@ def add_movie():
                  )
 
         current_app.db.movie.insert_one(asdict(movie))
-        return redirect(url_for(".index"))
+        current_app.db.user.update_one({"_id": session["user_id"]}, {"$push": {"movies": movie._id}})
+        return redirect(url_for(".movie", _id=movie._id))
 
     return render_template("new_movie.html", title="Movies Watchlist - Add Movie", form=form)
+
 
 @pages.get("/toggle-theme")
 def toggle_theme():
